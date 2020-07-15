cdff755b72d0766d3531eb4cc8df3c0dd00b83f0
serafima <svp1982@mail.ru>
2020-07-15 18:09:52 +0200

    Hello world exercis

---
+++ Week1/homework/js-exercises/helloWorld.js
@@ -0,0 +1,12 @@
+'use strict'
+
+console.log("Hello, world!"); //English
+console.log("Hola, Mundo!"); //Spanish
+console.log("Hallo, Wereld!"); //Dutch
+console.log("Привет мир!"); //Russian
+console.log("Hallo, Welt!"); //German
+console.log("Selam, Dünya!"); //Turkish
+console.log("Bonjour, le monde!"); //Franch
+console.log("Hej, världen!"); //Swedish
+console.log("Salve, mundi!"); //Latin
+console.log("Pozdrav svijete!"); //Croatian
