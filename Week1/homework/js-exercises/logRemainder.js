764e3c93bfad28f53ad1d6b4fc4db664d22a6add
serafima <svp1982@mail.ru>
2020-07-17 19:30:43 +0200

    Fix logRemainder exercise

--- Week1/homework/js-exercises/logRemainder.js
+++ Week1/homework/js-exercises/logRemainder.js
@@ -6,7 +6,7 @@ x = x % 3
 
 console.log(x);
 /* result is 1 because of remainder operator (%) returns the remainder left over when one operand is divided by a second operand.
-3*2 + 1 =7, remainder is 1
+(7/3=2,... not devide without remainder)3*2 + 1 =7, remainder is 1
 */
 let y = 21;
 
@@ -14,7 +14,7 @@ y = y % 4
 
 console.log(y);
 /* result is 1 because of remainder operator (%) returns the remainder left over when one operand is divided by a second operand.
-4*5 + 1 =21, remainder is 1
+(21/4=5,...not devide without remainder)4*5 + 1 =21, remainder is 1
 */
 
 let z = 13;
@@ -23,7 +23,7 @@ z = z % 2
 
 console.log(z);
 /* result is 1 because of remainder operator (%) returns the remainder left over when one operand is divided by a second operand.
-2*6 + 1 =13, is 1
+(13/2=6,... not devide without remainder)2*6 + 1 =13, remainder is 1
 */
 
 
